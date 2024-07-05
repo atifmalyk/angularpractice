81f2db3b29a741b69a5a1f54a15cebe77ce23d9a
atifmalyk <malikatifaheer@gmail.com>
2024-07-05 18:09:18 +0500

    creating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from "./rooms/rooms.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RoomsComponent]
 })
 export class AppComponent {
   title = 'my-angular-workspace';
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css'
+})
+export class RoomsComponent {
+  title="Hotel Rooms"
+  numberOfRooms=3
+
+}
