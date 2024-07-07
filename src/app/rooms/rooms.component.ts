19b3b140cc3301c22398c8f985e9b85c9602dadd
atifmalyk <atifhussain@gmail.com>
2024-07-07 20:59:18 +0500

    looping over users

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -5,10 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.css'
+  styleUrl: './rooms.component.css',
 })
 export class RoomsComponent {
   title="Hotel Rooms"
   numberOfRooms=3
-
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
+  ];
 }
