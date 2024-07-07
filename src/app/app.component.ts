4201d6be96a80a38fd1ca621822516a6f97c1edf
atifmalyk <atifhussain@gmail.com>
2024-07-07 22:55:49 +0500

    making module and calling in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
+import { LoginComponent } from './login/login.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RoomsComponent]
+    imports: [RouterOutlet, RoomsComponent,LoginComponent]
 })
 export class AppComponent {
   title = 'my-angular-workspace';
